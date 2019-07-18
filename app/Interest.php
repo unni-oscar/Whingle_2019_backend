62d6481568855557becf18132ffd2a674dd1c851
Unnikrishnan <unnikrishnanes@gmail.com>
2019-07-18 20:40:02 +0530

    167375571 showing interest received and sent

--- app/Interest.php
+++ app/Interest.php
@@ -9,4 +9,12 @@ class Interest extends Model
     const UNREAD = 0;
     const ACCEPT = 1;
     const REJECT = 2;
+    public function profileTo()
+    {
+        return $this->belongsTo('App\Profile', 'interest_to', 'id');
+    }
+    public function profileFrom()
+    {
+        return $this->belongsTo('App\Profile', 'interest_from', 'id');
+    }
 }
